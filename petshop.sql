165e769872796e79d2adb8ab9116898cf9e97b88
Juliano Costa Carvalho <79659336+Juliano-84@users.noreply.github.com>
2024-04-05 22:00:32 -0300

    Add files via upload

---
+++ petshop.sql
@@ -0,0 +1,199 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 06/04/2024 às 02:42
+-- Versão do servidor: 10.4.28-MariaDB
+-- Versão do PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `petshop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `animais`
+--
+
+CREATE TABLE `animais` (
+  `animal_id` int(10) NOT NULL,
+  `cliente_id` int(10) NOT NULL,
+  `especie` varchar(255) NOT NULL,
+  `raca` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `cliente_id` int(11) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `cpf` int(11) NOT NULL,
+  `endereco` varchar(100) NOT NULL,
+  `telefone` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura stand-in para view `cliente_animais`
+-- (Veja abaixo para a visão atual)
+--
+CREATE TABLE `cliente_animais` (
+`cliente_id` int(11)
+,`nome` varchar(50)
+,`num_animais` bigint(21)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `produto`
+--
+
+CREATE TABLE `produto` (
+  `produto_id` int(10) NOT NULL,
+  `cliente_id` int(10) NOT NULL,
+  `tipo` varchar(255) NOT NULL,
+  `quantidade` int(100) NOT NULL,
+  `valor` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `vendas`
+--
+
+CREATE TABLE `vendas` (
+  `venda_id` int(100) NOT NULL,
+  `cliente_id` int(10) NOT NULL,
+  `qtd_animais` int(100) NOT NULL,
+  `qtd_produtos` int(100) NOT NULL,
+  `valor` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Acionadores `vendas`
+--
+DELIMITER $$
+CREATE TRIGGER `registrar_venda_trigger` AFTER INSERT ON `vendas` FOR EACH ROW BEGIN
+DECLARE cliente_nome varchar(100);
+
+SELECT nome INTO cliente_nome FROM cliente WHERE cliente_id=NEW.cliente_id;
+INSERT INTO log_vendas(mensagem) VALUES (CONCAT('Nova venda resgistrada para o cliente', cliente_nome, '.Valor total:', NEW.valor));
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para view `cliente_animais`
+--
+DROP TABLE IF EXISTS `cliente_animais`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `cliente_animais`  AS SELECT `c`.`cliente_id` AS `cliente_id`, `c`.`nome` AS `nome`, (select count(0) from `animais` `a` where `a`.`cliente_id` = `c`.`cliente_id`) AS `num_animais` FROM `cliente` AS `c` ;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `animais`
+--
+ALTER TABLE `animais`
+  ADD PRIMARY KEY (`animal_id`),
+  ADD KEY `FK_animais_cliente` (`cliente_id`);
+
+--
+-- Índices de tabela `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`cliente_id`,`nome`);
+
+--
+-- Índices de tabela `produto`
+--
+ALTER TABLE `produto`
+  ADD PRIMARY KEY (`produto_id`,`tipo`),
+  ADD KEY `fk_cliente` (`cliente_id`);
+
+--
+-- Índices de tabela `vendas`
+--
+ALTER TABLE `vendas`
+  ADD PRIMARY KEY (`venda_id`),
+  ADD KEY `fk_cliente_2` (`cliente_id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `animais`
+--
+ALTER TABLE `animais`
+  MODIFY `animal_id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `cliente_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `produto`
+--
+ALTER TABLE `produto`
+  MODIFY `produto_id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `vendas`
+--
+ALTER TABLE `vendas`
+  MODIFY `venda_id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `animais`
+--
+ALTER TABLE `animais`
+  ADD CONSTRAINT `FK_animais_cliente` FOREIGN KEY (`cliente_id`) REFERENCES `cliente` (`cliente_id`);
+
+--
+-- Restrições para tabelas `produto`
+--
+ALTER TABLE `produto`
+  ADD CONSTRAINT `FK_produto_cliente` FOREIGN KEY (`cliente_id`) REFERENCES `cliente` (`cliente_id`),
+  ADD CONSTRAINT `fk_cliente` FOREIGN KEY (`cliente_id`) REFERENCES `cliente` (`cliente_id`);
+
+--
+-- Restrições para tabelas `vendas`
+--
+ALTER TABLE `vendas`
+  ADD CONSTRAINT `FK_vendas_cliente` FOREIGN KEY (`cliente_id`) REFERENCES `cliente` (`cliente_id`),
+  ADD CONSTRAINT `fk_cliente_2` FOREIGN KEY (`cliente_id`) REFERENCES `cliente` (`cliente_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
